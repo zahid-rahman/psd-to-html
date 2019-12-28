962f960b2cd69150a130c674dbca28d0a256c4cf
zahid-rahman <rahmanzahid94@gmail.com>
2019-12-29 00:05:11 +0600

    finishing full slider design and bootstrap fix

--- modus/js/main.js
+++ modus/js/main.js
@@ -1,32 +1,7 @@
 $(document).ready(function($){
-    $('.search-button').click(function(){
-
-        $(this).attr( function () { 
-             display:'none';
-        });
-
-        $('.search-field').attr( function () { 
-            display:'block';
-       });
-       
-    });
-
-
-
     $('.slider').slick({
         'autoplay':true,
-        'dots': true,
-        'infinite': true,
-        'speed': 500,
-        'autoplay': true,
-        'slidesToShow': 1,
-        'slidesToScroll': 1,
-        'arrows':true
-        
+        'arrows':true,
+        'dots':true
     });
-
-
-
-
-});
-
+})
