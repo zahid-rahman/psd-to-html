d9dc744e095356860a5b1f1eb3df29d5c618171b
zahid-rahman <rahmanzahid94@gmail.com>
2020-01-02 20:44:52 +0600

    adding new carousel slider

--- modus/js/main.js
+++ modus/js/main.js
@@ -4,4 +4,21 @@ $(document).ready(function($){
         'arrows':true,
         'dots':true
     });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        // margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
 })
